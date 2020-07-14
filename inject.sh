b1acdc7947d9346419fd28f6c437f030ed4f6a93
Michael van der Werve <mvdwerve@outlook.com>
2020-07-14 22:15:19 +0200

    missing fucking dot...

--- inject.sh
+++ inject.sh
@@ -2,4 +2,8 @@
 
 sed -i 's/add_subdirectory(cppcoreguidelines)/add_subdirectory\(copernica\)\nadd_subdirectory\(cppcoreguidelines\)/g' /llvm-project/clang-tools-extra/clang-tidy/CMakeLists.txt
 sed -i 's/  clangTidyCppCoreGuidelinesModule/  clangTidyCopernicaModule\n  clangTidyCppCoreGuidelinesModule/g' /llvm-project/clang-tools-extra/clang-tidy/CMakeLists.txt
-sed -i 's/This anchor is used to force the linker to link the CppCoreGuidelinesModule/\nextern volatile int CopernicaModuleAnchorSource; static int LLVM_ATTRIBUTE_UNUSED CopernicaModuleAnchorDestination = CopernicaModuleAnchorSource;/g' /llvm-project/clang-tools-extra/clang-tidy/ClangTidyForceLinker.h
+sed -i 's/This anchor is used to force the linker to link the CppCoreGuidelinesModule./\nextern volatile int CopernicaModuleAnchorSource; static int LLVM_ATTRIBUTE_UNUSED CopernicaModuleAnchorDestination = CopernicaModuleAnchorSource;/g' /llvm-project/clang-tools-extra/clang-tidy/ClangTidyForceLinker.h
+
+cat /llvm-project/clang-tools-extra/clang-tidy/ClangTidyForceLinker.h
+
+exit 1
