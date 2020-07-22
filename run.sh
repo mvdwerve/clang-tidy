09fb8bb0961c4e7804070c6243ce483bae8241de
Michael van der Werve <mvdwerve@outlook.com>
2020-07-22 12:10:38 +0200

    reduce to 18.04

--- run.sh
+++ run.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
-docker run -v `pwd`/project -v `pwd`/copernica:/llvm-project/clang-tools-extra/clang-tidy/copernica -v `pwd`/tests:/tests -it mvdwerve/clang-tidy bash
+mkdir -p build/
+docker run -v `pwd`/build:/build -v `pwd`/project -v `pwd`/copernica:/llvm-project/clang-tools-extra/clang-tidy/copernica -v `pwd`/tests:/tests -it mvdwerve/clang-tidy bash
